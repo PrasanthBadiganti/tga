780c367413283d90ceff1c3fc1f3cfa5430fda61
J V Naga Prasanth Badiganti <jvnagaprasanth.badiganti@publicissapient.com>
2023-12-19 11:04:02 +0530

    Updated

--- src/api/endpoint.py
+++ src/api/endpoint.py
@@ -2,16 +2,20 @@ from fastapi import APIRouter
 from fastapi import Request, Depends
 from fastapi.security import HTTPBearer
 from fastapi_ratelimit import limiter
+from prometheus_client import Counter
 from starlette.responses import JSONResponse
 
 from src.service.user_service import UserService
 
 router = APIRouter(tags=["ops"], dependencies=[Depends(HTTPBearer())])
 
+requests_counter = Counter('my_requests_total', 'Total number of requests')
+
 
 @router.get("/fetch-users")
 @limiter.limit("2/minute")
 async def fetch_users(request: Request):
+    requests_counter.inc()
     try:
         service = UserService()
         return service.fetch_all_users(request)
